abc57504a38acf517a6c3e043bb7352323cbd0f2
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-15 15:19:32 +0100

    commento in js per il lavoro da svolgere nell'es.1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* ESERCIZIO 1 */
+/* output in pagina e console per restituire il numero randomico generato al click di un bottone*/
+/* 1)seleziono i due elementi html in cui far comparire il numero selezionato */
+/* 2) addevent listener al bottone. inserisco dentro tutto ciò che elenco qui sotto */
+/* 3) genero due numeri casuali e li inserisco all'interno di html */
+/* 4) creo condizione in cui stabilisco il numero vincitore */
+/* 4) applico eventuali elementi di stile alla pagina */
+
+
+/* ESERCIZIO 1 */
